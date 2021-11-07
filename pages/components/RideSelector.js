ac69a55583c83a38ffe0116b1dec1c722556cabd
Devlin <devlincodes@gmail.com>
2021-11-06 21:47:45 -0700

    Finds routes and calculates price

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,26 +1,41 @@
+import { useEffect, useState } from 'react';
 import tw from 'tailwind-styled-components'
-import { carList } from '../data/carList';
+import { carList } from '../../data/carList';
 
-function RideSelector() {
+function RideSelector({pickupCoordinates, dropoffCoordinates}) {
+
+    const [rideDuration, setRideDuration] = useState(0);
+
+    useEffect(() => {
+        fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]}, ${pickupCoordinates[1]};${dropoffCoordinates[0]}, ${dropoffCoordinates[1]}?access_token=pk.eyJ1IjoiZGV2bGlucm9jaGEiLCJhIjoiY2t2bG82eTk4NXFrcDJvcXBsemZzdnJoYSJ9.aq3RAvhuRww7R_7q-giWpA`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.routes) {
+                    setRideDuration(data.routes[0].duration / 100);
+                };
+            })
+    }, [pickupCoordinates, dropoffCoordinates]);
+
     return (
         <Wrapper>
             <Title>Choose a ride, or swipe up for more</Title>
             <CarList>
-                {carList.map((car, index)=>(
+                {carList.map((car, index) => (
                     <Car key={index} >
 
                         <CarImage src={car.imgUrl} />
 
                         <CarDetails>
+
                             <Service>{car.service}</Service>
                             <Time>5 min away</Time>
+
                         </CarDetails>
                     
-                        <Price>$24.00</Price>
+                        <Price>${(rideDuration * car.multiplier).toFixed(2)}</Price>
                     </Car>
                 ))}
 
-            
             </CarList>
         </Wrapper>
     )
